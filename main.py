5ada426d230389d508a95ac81a84e45347326640
OZKON24 <za9474@yahoo.com>
2024-12-06 01:45:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def animated_marker():
     bar = progressbar.ProgressBar(widgets=widgets).start()
       
     for i in range(50):
-        time.sleep(0.1)
+        time.sleep(1)
         bar.update(i)
           
 animated_marker()
